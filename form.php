58ce126af927bd54930bf2f2778e9217e93afa03
Aloïs Delaune <90683963+AloisD@users.noreply.github.com>
2021-11-03 12:10:00 +0100

    Update form.php

--- form.php
+++ form.php
@@ -1,19 +1,19 @@
-<form  action="thanks.php"  method="post">
+<form action="thanks.php" method="post">
     <div>
-        <label  for="nom">Nom :</label>
-        <input  type="text"  id="nom"  name="user_name">
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="user_name">
     </div>
     <div>
-        <label  for="prénom">Prénom :</label>
-        <input  type="text"  id="prénom"  name="user_firstname">
+        <label for="prénom">Prénom :</label>
+        <input type="text" id="prénom" name="user_firstname">
     </div>
     <div>
-        <label  for="courriel">Courriel :</label>
-        <input  type="email"  id="courriel"  name="user_email">
+        <label for="courriel">Courriel :</label>
+        <input type="email" id="courriel" name="user_email">
     </div>
     <div>
-        <label  for="téléphone">Téléphone :</label>
-        <input  type="tel"  id="téléphone"  name="user_tel">
+        <label for="téléphone">Téléphone :</label>
+        <input type="tel" id="téléphone" name="user_tel">
     </div>
     <div>
         <label for="sujet">Sujet :</label>
@@ -25,10 +25,10 @@
         </select>
     </div>
     <div>
-        <label  for="message">Message :</label>
-        <textarea  id="message"  name="user_message"></textarea>
+        <label for="message">Message :</label>
+        <textarea id="message" name="user_message"></textarea>
     </div>
-    <div  class="button">
-        <button  type="submit">Envoyer votre message</button>
+    <div class="button">
+        <button type="submit">Envoyer votre message</button>
     </div>
 </form>
